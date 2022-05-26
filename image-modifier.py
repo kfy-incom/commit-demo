c9ca0111fed2045257d24e078688b011a251a4c4
Victoria Dye <vdye@github.com>
2022-05-26 12:46:00 -0700

    Add --output option

--- image-modifier.py
+++ image-modifier.py
@@ -9,6 +9,12 @@ def main():
     parser = argparse.ArgumentParser(description='Manipulate and display images.')
     parser.add_argument('image', type=str,
                         help='the image to process')
+    parser.add_argument('--output', '-o', metavar="path",
+                        help='path to save the image')
+    parser.add_argument('--invert', '-i', action='store_true',
+                        help='invert the colors in the image')
+    parser.add_argument('--grey', '-g', action='store_true',
+                        help='convert the image to greyscale')
     args = parser.parse_args()
 
     # Verify file validity
@@ -22,6 +28,13 @@ def main():
     plt.imshow(img, cmap='gray', vmin=0, vmax=255)
     plt.axis('off')
     plt.show()
+
+    if args.output:
+        if not os.path.isdir(args.output):
+            os.makedirs(args.output, exist_ok=True)
+        img_basename = os.path.basename(args.image)
+        plt.imsave(os.path.join(args.output, img_basename), img,
+                   cmap='gray', vmin=0, vmax=255)
 
 if __name__ == "__main__":
     main()
